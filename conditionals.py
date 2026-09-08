51f125a83304da168595e751d368fdfda52adad1
Kingtheworldsayhipy <huynhuyhuynh22@gmail.com>
2026-09-08 18:32:47 +0700

    alo

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+print(' we gonna do about conditionals')
+# same as math we have so many ways to do conditionals in python
+# i arleady know about if else and elif statements so i am gonna skip it 
+#if else statements are used to execute a block of code if a condition is true, and another block of code if the condition is false. The elif statement allows you to check multiple conditions in sequence.
+score = int(input("Enter your score: "))
+if score >= 90:
+    print("You got an A!")
+elif score >= 80:
+    print("You got a B!")
+else:
+    print("You got a C or lower.")
+
